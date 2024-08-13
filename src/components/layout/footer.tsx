8891d117db7a9258be581c35a6844a8db934bc9b
Ender Puentes <ender.puentes@aerolab.co>
2024-08-13 17:44:58 -0300

    feat: footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,10 +1,13 @@
 type Props = {};
 
 export default function Footer({}: Props) {
+  const date = new Date();
   return (
     <footer className="h-10 flex justify-center items-center">
       <div className="container p-2 flex justify-center items-center">
-        <p className="text-xs">Endev - Copyright 2024</p>
+        <p className="text-xs">
+          Ender Puentes - Copyright {date.getFullYear()}
+        </p>
       </div>
     </footer>
   );
