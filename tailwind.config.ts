707e00cf3df0cbbec816b10e4fa54de335b0a693
Ender Puentes <endpuent@gmail.com>
2024-08-14 15:55:02 -0300

    feat: install shadcnui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,9 +11,7 @@ const config: Config = {
   theme: {
     container: {
       center: true,
-      padding: {
-        DEFAULT: '1rem',
-      },
+      padding: '2rem',
       screens: {
         '2xl': '1400px',
       },
@@ -55,9 +53,19 @@ const config: Config = {
         },
       },
       borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
         sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
@@ -65,6 +73,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 };
 export default config;
