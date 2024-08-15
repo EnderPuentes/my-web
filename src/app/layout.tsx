c3b7f56fa40995d5c7512c889c1436e089ac1208
Ender Puentes <endpuent@gmail.com>
2024-08-15 12:16:57 -0300

    feat: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,24 @@ const jetBrainsMono = JetBrains_Mono({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Ender Puentes',
+  applicationName: 'Ender Puentes',
   description: 'Software Developer',
+  openGraph: {
+    type: 'website',
+    url: new URL(process.env.NEXT_PUBLIC_BASE_URL ?? ''),
+    title: 'Ender Puentes',
+    description: 'Software Developer',
+    siteName: 'Ender Puentes',
+    images: [
+      {
+        url: new URL(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/favicons/favicon.webp` ??
+            '/favicons/favicon.webp'
+        ),
+      },
+    ],
+  },
+  metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL ?? ''),
 };
 
 export default function RootLayout({
