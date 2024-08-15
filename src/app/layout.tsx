215f68b0fdc7e61db46f7f233a74c909247bf52d
Ender Puentes <endpuent@gmail.com>
2024-08-15 10:45:47 -0300

    revert chore: remove captcha provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Header from '@/components/layout/header';
 import ThemeProvider from '@/components/providers/theme';
 import { Toaster } from '@/components/ui/toaster';
 import type { Metadata } from 'next';
+import { ReCaptchaProvider } from 'next-recaptcha-v3';
 import { JetBrains_Mono } from 'next/font/google';
 
 const jetBrainsMono = JetBrains_Mono({ subsets: ['latin'] });
@@ -21,12 +22,14 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={`${jetBrainsMono.className} flex flex-col min-h-screen`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <Header />
-          <main className="flex-1">{children}</main>
-          <Footer />
-          <Toaster />
-        </ThemeProvider>
+        <ReCaptchaProvider reCaptchaKey={process.env.RECAPTCHA_SITE_KEY}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <Header />
+            <main className="flex-1">{children}</main>
+            <Footer />
+            <Toaster />
+          </ThemeProvider>
+        </ReCaptchaProvider>
       </body>
     </html>
   );
