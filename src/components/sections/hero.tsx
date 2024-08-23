028b5f782682b9d8eb94c365f428a8a0ce59d1b1
Ender Puentes <ender.puentes@aerolab.co>
2024-08-23 16:57:34 -0300

    fix: styles

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -32,14 +32,14 @@ export default function Hero({ text }: Props) {
   return (
     <div className="flex justify-center items-center bg-slate-100 dark:bg-gray-950 py-5 mb-5 sm:mb-10">
       <div className="container">
-        <div className="bg-black rounded-lg shadow-lg overflow-hidden w-full h-[220px] md:h-[250px]">
+        <div className="bg-black rounded-lg shadow-lg overflow-hidden w-full h-[240px] md:h-[280px]">
           <div className="bg-gray-800 text-gray-300 p-3 flex items-center space-x-2 w-full">
             <span className="block w-3 h-3 rounded-full bg-red-500"></span>
             <span className="block w-3 h-3 rounded-full bg-yellow-500"></span>
             <span className="block w-3 h-3 rounded-full bg-green-500"></span>
           </div>
           <div className="p-6">
-            <pre className="text-green-500 font-mono whitespace-pre-wrap text-xs sm:text-base flex justify-start items-start gap-0">
+            <pre className="text-green-500 font-mono whitespace-pre-wrap text-xs sm:text-base leading-5 sm:leading-7 flex justify-start items-start gap-0">
               <span>{'~/ '}</span>
               <span>
                 {currentText}
