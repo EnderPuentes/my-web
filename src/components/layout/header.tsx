0b7458b8dc84e450325a95e8df8d3117a055ab42
Ender Puentes <endpuent@gmail.com>
2024-08-26 20:33:01 -0300

    feat: add lang switch

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,15 +1,15 @@
-'use client';
-
 import Logo from '@/components/layout/logo';
 import ThemeToggler from '@/components/layout/theme-toggler';
 import { Locale } from '@/types/locales';
 import Link from 'next/link';
+import LangToggler from './lang-toggler';
 
 type Props = {
   t: Locale['layout']['header'];
+  lang: string;
 };
 
-export default function Header({ t }: Props) {
+export default function Header({ t, lang }: Props) {
   return (
     <header className="sticky top-0 border-b bg-background dark:bg-background to-gray-950 z-50 shadow-sm h-20 flex justify-center items-center">
       <div className="container flex justify-between items-center">
@@ -17,17 +17,18 @@ export default function Header({ t }: Props) {
           <Logo />
         </Link>
         <div className="flex justify-between items-center gap-6">
-          <div className="flex justify-between items-center gap-3">
-            <Link
-              aria-label={t.menu.logbook}
-              className="text-base"
-              href="/logbook"
-              title={t.menu.logbook}
-            >
-              {t.menu.logbook}
-            </Link>
+          <Link
+            aria-label={t.menu.logbook}
+            className="text-base"
+            href="/logbook"
+            title={t.menu.logbook}
+          >
+            {t.menu.logbook}
+          </Link>
+          <div className="flex items-center gap-1">
+            <LangToggler lang={lang} />
+            <ThemeToggler />
           </div>
-          <ThemeToggler />
         </div>
       </div>
     </header>
