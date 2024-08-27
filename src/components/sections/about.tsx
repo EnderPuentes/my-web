1df370af89cdd41cf7f92f656742de62d69c8527
Ender Puentes <endpuent@gmail.com>
2024-08-27 11:25:20 -0300

    fix: padding about section

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -5,7 +5,7 @@ type Props = { t: Locale['pages']['home']['about'] };
 export default async function About({ t }: Props) {
   return (
     <section className="mb-10">
-      <div className="container flex flex-col justify-start items-start gap-5">
+      <div className="container flex flex-col justify-start items-start gap-5 px-5">
         <h2 className="font-semibold text-lg sm:text-xl">{t.title}</h2>
         <div className="w-full grid gap-4">
           <p className="text-xs sm:text-base leading-6 sm:leading-7 dark:text-gray-300">
