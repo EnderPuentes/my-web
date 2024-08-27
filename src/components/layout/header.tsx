f7be9be17e6010a12ead867c26a4c42c7429f6a8
Ender Puentes <ender.puentes@aerolab.co>
2024-08-27 17:49:30 -0300

    fix: links header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -14,7 +14,7 @@ export default function Header({ t, lang }: Props) {
     <header className="sticky top-0 border-b bg-background dark:bg-background to-gray-950 z-50 shadow-sm h-20 flex justify-center items-center">
       <div className="container flex justify-between items-center">
         <Link
-          href={`${lang}/`}
+          href={`/${lang}/`}
           className="flex justify-start items-start flex-col"
         >
           <Logo />
@@ -23,7 +23,7 @@ export default function Header({ t, lang }: Props) {
           <Link
             aria-label={t.menu.logbook}
             className="text-base"
-            href={`${lang}/logbook`}
+            href={`/${lang}/logbook`}
             title={t.menu.logbook}
           >
             {t.menu.logbook}
