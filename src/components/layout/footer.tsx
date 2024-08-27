271bbe5bf9c7adfd1328bda87f5600bc6c79f551
Ender Puentes <ender.puentes@aerolab.co>
2024-08-27 01:04:10 -0300

    feat: add socials medias

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,6 +1,11 @@
 import { Locale } from '@/types/locales';
 import Link from 'next/link';
-import { PiGithubLogoBold, PiLinkedinLogoBold } from 'react-icons/pi';
+import {
+  PiGithubLogoBold,
+  PiLinkedinLogoBold,
+  PiTelegramLogoBold,
+  PiXLogoBold,
+} from 'react-icons/pi';
 
 type Props = { t: Locale['layout']['footer'] };
 
@@ -9,11 +14,13 @@ export default function Footer({ t }: Props) {
   return (
     <footer className="flex flex-col justify-center items-center gap-10">
       <div className="flex flex-col justify-center items-center gap-10 py-10 px-5">
-        <h1 className="text-xl text-center">{t.socialMedia.title}</h1>
+        <h1 className="text-lg sm:text-xl text-center">
+          {t.socialMedia.title}
+        </h1>
         <div className="flex justify-center items-center gap-4">
           <Link
             aria-label="Linkedin"
-            className="text-4xl"
+            className="text-3xl sm:text-4xl"
             target="_blank"
             href="https://www.linkedin.com/in/enderpuentes/"
           >
@@ -21,11 +28,27 @@ export default function Footer({ t }: Props) {
           </Link>
           <Link
             aria-label="Github"
-            className="text-4xl"
+            className="text-3xl sm:text-4xl"
             target="_blank"
-            href="https://github.com/EnderPuentes"
+            href="https://github.com/enderpuentes"
           >
             <PiGithubLogoBold />
+          </Link>
+          <Link
+            aria-label="Telegram"
+            className="text-3xl sm:text-4xl"
+            target="_blank"
+            href="https://t.me/enderpuentes"
+          >
+            <PiTelegramLogoBold />
+          </Link>
+          <Link
+            aria-label="X"
+            className="text-3xl sm:text-4xl"
+            target="_blank"
+            href="https://x.com/enderpuents"
+          >
+            <PiXLogoBold />
           </Link>
         </div>
       </div>
