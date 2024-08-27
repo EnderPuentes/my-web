b6bd530dd4c2bd398e41df60a57697f6cff5704e
Ender Puentes <endpuent@gmail.com>
2024-08-27 11:01:55 -0300

    chore: metatada

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -7,10 +7,15 @@ import { Toaster } from '@/components/ui/toaster';
 import { Locale } from '@/types/locales';
 import { getLocale } from '@/utils/locales';
 import { GoogleAnalytics } from '@next/third-parties/google';
+import { Viewport } from 'next';
 import { ReCaptchaProvider } from 'next-recaptcha-v3';
 import { JetBrains_Mono } from 'next/font/google';
 
 const jetBrainsMono = JetBrains_Mono({ subsets: ['latin'] });
+
+export const viewport: Viewport = {
+  themeColor: '#8A2BE2',
+};
 
 export default async function RootLayout({
   children,
