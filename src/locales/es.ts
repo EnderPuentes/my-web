7493f3ffcbf58cfb88f926acaa04865fae023f9f
Ender Puentes <ender.puentes@aerolab.co>
2024-08-27 11:40:44 -0300

    fix: about section description

--- src/locales/es.ts
+++ src/locales/es.ts
@@ -14,7 +14,8 @@ const es: Locale = {
       },
       about: {
         title: 'Sobre mí',
-        description: '',
+        description:
+          'Soy un desarrollador proactivo y apasionado con 8 años de carrera profesional en el campo del desarrollo de software. He trabajado con diversos lenguajes de programación, enfocado principalmente en el desarrollo de aplicaciones web. Mi experiencia incluye la creación de soluciones técnicas sólidas y escalables, abordando desafíos de manera creativa y eficiente. He descubierto una gran pasión por los productos digitales, y me esfuerzo por garantizar que cada característica que desarrollo no solo cumpla, sino que supere las expectativas. Destaco por mi habilidad para colaborar en equipos multidisciplinarios y trabajar bajo presión, mostrando empatía y un firme compromiso con los objetivos del proyecto.',
       },
     },
     logbook: {
