fcf16027f09f104ee4124da38094150340ad81dc
Ender Puentes <endpuent@gmail.com>
2024-08-29 19:26:59 -0300

    feat: remove getLocale function deprecated

--- src/app/[lang]/not-found.tsx
+++ src/app/[lang]/not-found.tsx
@@ -1,7 +1,4 @@
-import { Locale } from '@/types/locales';
-import { getLocale } from '@/utils/locales';
 import { Metadata } from 'next';
-import Link from 'next/link';
 
 export async function generateMetadata(): Promise<Metadata> {
   return {};
@@ -24,29 +21,30 @@ function Start() {
 }
 
 export default async function NotFound() {
-  const lang = 'en';
-  const t: Locale = await getLocale(lang);
+  return <></>;
+  // const lang = 'en';
+  // const t: Locale = await getLocale(lang);
 
-  return (
-    <section className="relative w-full h-screen overflow-hidden flex flex-col justify-center items-center text-center -mt-20">
-      <div className="text">
-        <h2 className="text-xl font-bold mb-5">{t.pages.notFound.title}</h2>
-        <p className="text-xs sm:text-base">{t.pages.notFound.description}</p>
+  // return (
+  //   <section className="relative w-full h-screen overflow-hidden flex flex-col justify-center items-center text-center -mt-20">
+  //     <div className="text">
+  //       <h2 className="text-xl font-bold mb-5">{t.pages.notFound.title}</h2>
+  //       <p className="text-xs sm:text-base">{t.pages.notFound.description}</p>
 
-        <Link
-          className="inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2 max-w-80 mx-auto mt-5 sm:mt-10 text-xs sm:text-sm"
-          href={`/${lang}/logbook`}
-        >
-          {t.pages.notFound.back}
-        </Link>
-      </div>
-      <div className="absolute top-0 left-0 w-full h-full">
-        {[...Array(200)].map((_, i) => (
-          <Start key={i} />
-        ))}
-      </div>
+  //       <Link
+  //         className="inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2 max-w-80 mx-auto mt-5 sm:mt-10 text-xs sm:text-sm"
+  //         href={`/${lang}/logbook`}
+  //       >
+  //         {t.pages.notFound.back}
+  //       </Link>
+  //     </div>
+  //     <div className="absolute top-0 left-0 w-full h-full">
+  //       {[...Array(200)].map((_, i) => (
+  //         <Start key={i} />
+  //       ))}
+  //     </div>
 
-      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-br from-purple-900 via-indigo-700 to-black opacity-70 mix-blend-overlay"></div>
-    </section>
-  );
+  //     <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-br from-purple-900 via-indigo-700 to-black opacity-70 mix-blend-overlay"></div>
+  //   </section>
+  // );
 }
