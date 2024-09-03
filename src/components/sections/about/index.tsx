75d4e9b1145c1ae94faf6954148c2f37565f2e18
Ender Puentes <endpuent@gmail.com>
2024-09-02 22:04:04 -0300

    chore: styles contact section

--- src/components/sections/about/index.tsx
+++ src/components/sections/about/index.tsx
@@ -5,8 +5,10 @@ type Props = { data: AboutSchema };
 export default async function About({ data }: Props) {
   return (
     <section className="mb-10">
-      <div className="container flex flex-col justify-start items-start gap-5 px-5 py-28">
-        <h2 className="font-semibold text-lg sm:text-xl">{data.title}</h2>
+      <div className="container flex flex-col justify-start items-start gap-5 px-5 mt-16 sm:mt-28">
+        <h2 className="font-semibold text-lg sm:text-2xl sm:mb-5">
+          {data.title}
+        </h2>
         <div className="w-full grid gap-4">
           <p
             className="text-xs sm:text-base leading-6 sm:leading-7 dark:text-gray-300"
