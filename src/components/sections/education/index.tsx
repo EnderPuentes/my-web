2b637e03d7d5cf37512ecd47169e244ea0e60741
Ender Puentes <endpuent@gmail.com>
2024-09-11 17:14:12 -0300

    feat: add degree component

--- src/components/sections/education/index.tsx
+++ src/components/sections/education/index.tsx
@@ -1,5 +1,32 @@
-import { EducationSchema } from '@/services/sanity/parser';
+import { DegreeSchema, EducationSchema } from '@/services/sanity/parser';
 import Link from 'next/link';
+
+type DegreeProps = {
+  degree: DegreeSchema;
+};
+
+function Degree({ degree }: DegreeProps) {
+  return (
+    <div className="flex flex-col justify-start items-start gap-1 mb-2 border-b py-5 w-full !m-0">
+      {degree.school.website ? (
+        <Link
+          aria-label={degree.school.name}
+          href={degree.school.website}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-base sm:text-lg"
+        >
+          <span>{degree.school.name}</span>
+        </Link>
+      ) : (
+        <p className="text-base sm:text-lg">{degree.school.name}</p>
+      )}
+      <p className="text-xs sm:text-base font-light dark:text-gray-300">
+        {degree.title}, {degree.startDate} - {degree.endDate}
+      </p>
+    </div>
+  );
+}
 
 type Props = { data: EducationSchema };
 
@@ -11,27 +38,8 @@ export default function Education({ data }: Props) {
           {data.title}
         </h2>
         <div className="flex flex-col gap-0 justify-start items-start w-full">
-          {data.degrees.map((degree) => (
-            <>
-              <div className="flex flex-col justify-start items-start gap-1 mb-2 border-b py-5 w-full !m-0">
-                {degree.school.website ? (
-                  <Link
-                    aria-label={degree.school.name}
-                    href={degree.school.website}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-base sm:text-lg"
-                  >
-                    <span>{degree.school.name}</span>
-                  </Link>
-                ) : (
-                  <p className="text-base sm:text-lg">{degree.school.name}</p>
-                )}
-                <p className="text-xs sm:text-base font-light dark:text-gray-300">
-                  {degree.title}, {degree.startDate} - {degree.endDate}
-                </p>
-              </div>
-            </>
+          {data.degrees.map((degree, iDegree) => (
+            <Degree key={iDegree} degree={degree} />
           ))}
         </div>
       </div>
