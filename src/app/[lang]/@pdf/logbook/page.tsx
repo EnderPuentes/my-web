a4007aac1bfcadb8f142d8c1f68b6d47d3615ef6
Ender Puentes <endpuent@gmail.com>
2024-09-11 23:09:41 -0300

    feat: add logbook page base  version pdf

---
+++ src/app/[lang]/@pdf/logbook/page.tsx
@@ -0,0 +1,50 @@
+import { Education } from '@/components/sections/logbook/education';
+import { Expertise } from '@/components/sections/logbook/expertise';
+import { Identity } from '@/components/sections/logbook/identity';
+import { Skills } from '@/components/sections/logbook/skills';
+import { getLogbookPage } from '@/services/sanity/request';
+import { Metadata } from 'next';
+
+type Props = {
+  params: {
+    lang: 'en' | 'es';
+  };
+};
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const data = await getLogbookPage(params.lang);
+
+  return {
+    title: data?.meta.title,
+    description: data?.meta.description,
+    openGraph: {
+      title: data?.meta.title,
+      description: data?.meta.description,
+    },
+    metadataBase: new URL(`${BASE_URL}/${params.lang}/logbook`),
+    keywords: data?.meta.keywords?.join(', ') || '',
+  };
+}
+
+export default async function LogbookPDF({ params }: Props) {
+  const data = await getLogbookPage(params.lang);
+  return (
+    <>
+      <h1>Version PDF</h1>
+      {data?.sections.map((section) => {
+        switch (section._type) {
+          case 'identity':
+            return <Identity key={section._key} data={section} />;
+          case 'expertise':
+            return <Expertise key={section._key} data={section} />;
+          case 'skills':
+            return <Skills key={section._key} data={section} />;
+          case 'education':
+            return <Education key={section._key} data={section} />;
+        }
+      })}
+    </>
+  );
+}
