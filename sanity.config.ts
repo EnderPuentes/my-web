ddfe82b92cd9cf3f98154a181ebd51d3f2f937eb
Ender Puentes <endpuent@gmail.com>
2024-09-23 22:29:16 -0300

    feat(sanity/config): add articleSchema to internationalization plugin config

--- sanity.config.ts
+++ sanity.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
         { id: 'en', title: 'English' },
         { id: 'es', title: 'Spanish' },
       ],
-      schemaTypes: ['home', 'logbook', 'notFound', 'layout'],
+      schemaTypes: ['article', 'home', 'logbook', 'notFound', 'layout'],
     }),
   ],
 });
