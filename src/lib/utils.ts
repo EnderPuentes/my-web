987ae2a0e2ae96036a6b7b369b6a07535f14c55f
Ender Puentes <endpuent@gmail.com>
2024-09-25 00:01:42 -0300

    feat(utils): add formatDateByLang method

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,21 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatDateByLang(date: string, lang: 'en' | 'es') {
+  // Check if the language is Spanish (Latin America)
+  if (lang === 'es') {
+    return new Intl.DateTimeFormat('es-ES', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    }).format(new Date(date));
+  }
+
+  // Default to US English if no language is specified
+  return new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }).format(new Date(date));
+}
