1a06994d36eabb430005dd1720c7b54982d610f0
Ender Puentes <endpuent@gmail.com>
2024-09-25 00:01:10 -0300

    feat(section/featuredArticles): add item article component

--- src/components/sections/home/featuredArticles/index.tsx
+++ src/components/sections/home/featuredArticles/index.tsx
@@ -1,20 +1,45 @@
-import { FeaturedArticlesSchema } from '@/services/sanity/parser';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { formatDateByLang } from '@/lib/utils';
+import {
+  FeaturedArticlesItemSchema,
+  FeaturedArticlesSchema,
+} from '@/services/sanity/parser';
 import Link from 'next/link';
+
+type ItemProps = { data: FeaturedArticlesItemSchema; lang: 'en' | 'es' };
+
+function FeaturedArticlesItem({ data, lang }: ItemProps) {
+  return (
+    <Link href={`/${lang}/blog/${data.slug}`}>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-lg font-semibold">{data.title}</CardTitle>
+        </CardHeader>
+        <CardContent className="flex flex-col items-end justify-start w-full gap-3">
+          <p className="text-xs sm:text-base leading-6 sm:leading-7 dark:text-gray-300 w-full">
+            {data.summary}
+          </p>
+          <span className="text-2xs">
+            {formatDateByLang(data.updateAt, lang)}
+          </span>
+        </CardContent>
+      </Card>
+    </Link>
+  );
+}
 
 type Props = { data: FeaturedArticlesSchema; lang: 'en' | 'es' };
 
-export async function FeaturedArticles({ data, lang }: Props) {
+export function FeaturedArticles({ data, lang }: Props) {
   return (
-    <section className="mb-10">
+    <section className="mb-20">
       <div className="container flex flex-col justify-start items-start gap-5">
         <h2 className="font-semibold text-lg sm:text-2xl sm:mb-5">
           {data.title}
         </h2>
-        <div className="">
+        <div className="flex flex-col justify-start items-start gap-5">
           {data.items.map((item) => (
-            <Link key={item._key} href={`/${lang}/blog/${item.slug}`}>
-              {item.title}
-            </Link>
+            <FeaturedArticlesItem key={item._key} data={item} lang={lang} />
           ))}
         </div>
       </div>
