b8b7659f02116d1ca1ab0ae6f4b31e4bf1c3e232
Ender Puentes <endpuent@gmail.com>
2024-09-30 12:49:37 -0300

    feat(HeadArticle): add h1 styles

--- src/components/sections/article/head/index.tsx
+++ src/components/sections/article/head/index.tsx
@@ -7,7 +7,9 @@ export function ArticleHead({ title, publishAt }: Props) {
   return (
     <section className="my-10">
       <div className="container">
-        <h1 className="font-semibold text-lg sm:text-3xl sm:mb-5">{title}</h1>
+        <h1 className="font-semibold text-lg sm:text-3xl leading-[150%] sm:leading-[170%] mb-5 sm:mb-10">
+          {title}
+        </h1>
         <span className="text-2xs">{publishAt}</span>
         <span className="border-b w-full block mt-5" />
       </div>
