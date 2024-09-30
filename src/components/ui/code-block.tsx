e98e4723630f11850de19054e682fe15d22e6bc0
Ender Puentes <endpuent@gmail.com>
2024-09-30 12:50:59 -0300

    feat(code-block): refactor props

--- src/components/ui/code-block.tsx
+++ src/components/ui/code-block.tsx
@@ -1,8 +1,15 @@
 'use client';
-
 import { CopyBlock, hybrid } from 'react-code-blocks';
 import { CopyBlockProps } from 'react-code-blocks/dist/components/CopyBlock';
 
-export function CodeBlock(props: CopyBlockProps) {
-  return <CopyBlock theme={hybrid} codeBlock {...props} />;
+export function CodeBlock({ text, language }: CopyBlockProps) {
+  return (
+    <CopyBlock
+      codeBlock
+      showLineNumbers
+      theme={hybrid}
+      text={text}
+      language={language}
+    />
+  );
 }
