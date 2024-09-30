6662543491da2a309d0c5939f16b7c39a96dc8b7
Ender Puentes <endpuent@gmail.com>
2024-09-30 13:39:57 -0300

    feat(mutilcontent): replace react-code-block for react-refractor in code-block component

--- src/components/ui/code-block.tsx
+++ src/components/ui/code-block.tsx
@@ -1,15 +1,30 @@
-'use client';
-import { CopyBlock, hybrid } from 'react-code-blocks';
-import { CopyBlockProps } from 'react-code-blocks/dist/components/CopyBlock';
+import { Refractor, registerLanguage } from 'react-refractor';
 
-export function CodeBlock({ text, language }: CopyBlockProps) {
+import bash from 'refractor/lang/bash';
+import js from 'refractor/lang/javascript';
+import jsx from 'refractor/lang/jsx';
+import python from 'refractor/lang/python';
+import tsx from 'refractor/lang/tsx';
+import typescript from 'refractor/lang/typescript';
+
+import 'prism-themes/themes/prism-nord.css';
+
+registerLanguage(bash);
+registerLanguage(js);
+registerLanguage(jsx);
+registerLanguage(tsx);
+registerLanguage(typescript);
+registerLanguage(python);
+
+type Props = {
+  code: string;
+  language: string;
+};
+
+export function CodeBlock({ code, language }: Props) {
   return (
-    <CopyBlock
-      codeBlock
-      showLineNumbers
-      theme={hybrid}
-      text={text}
-      language={language}
-    />
+    <div className="text-xs sm:text-base mb-5">
+      <Refractor language={language} value={code} />
+    </div>
   );
 }
