8e4dbe76cb0cfe35d650cf1c289237bfc060c5d4
Ender Puentes <endpuent@gmail.com>
2024-09-30 18:32:31 -0300

    feat(sitemap): add articles blog pages

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,28 +1,52 @@
+import { getBlogArticlesForSitemap } from '@/services/sanity/request';
 import { MetadataRoute } from 'next';
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? 'https://enderpuentes.com';
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: `${BASE_URL}/en`,
-      lastModified: new Date(),
-      priority: 1,
-    },
-    {
-      url: `${BASE_URL}/es`,
-      lastModified: new Date(),
-      priority: 1,
-    },
-    {
-      url: `${BASE_URL}/en/logbook`,
-      lastModified: new Date(),
-      priority: 0.9,
-    },
-    {
-      url: `${BASE_URL}/es/logbook`,
-      lastModified: new Date(),
-      priority: 0.9,
-    },
-  ];
+type Item = {
+  url: string;
+  lastModified: string;
+  priority: number;
+};
+
+const items: Item[] = [
+  {
+    url: `${BASE_URL}/en`,
+    lastModified: new Date().toISOString(),
+    priority: 1,
+  },
+  {
+    url: `${BASE_URL}/es`,
+    lastModified: new Date().toISOString(),
+    priority: 1,
+  },
+  {
+    url: `${BASE_URL}/en/logbook`,
+    lastModified: new Date().toISOString(),
+    priority: 0.9,
+  },
+  {
+    url: `${BASE_URL}/es/logbook`,
+    lastModified: new Date().toISOString(),
+    priority: 0.9,
+  },
+];
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const blogArticles = await getBlogArticlesForSitemap();
+
+  blogArticles?.forEach((article) => {
+    const url = `${BASE_URL}/${article.lang}/${article.slug}`;
+    const urlExist = items.find((item) => item.url === url);
+
+    if (urlExist) return;
+
+    items.push({
+      url,
+      lastModified: new Date().toISOString(),
+      priority: 0.7,
+    });
+  });
+
+  return items;
 }
