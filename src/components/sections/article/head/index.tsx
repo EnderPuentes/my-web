8a8b72bdeee88576698db26cbc5cddaf900eb43d
Ender Puentes <endpuent@gmail.com>
2024-10-01 13:51:41 -0300

    feat(components/sections/ArticleHead): refactor detail article and add CopyUrlComponent

--- src/components/sections/article/head/index.tsx
+++ src/components/sections/article/head/index.tsx
@@ -1,3 +1,5 @@
+import { CopyUrlButton } from "@/components/ui/action-button";
+
 type Props = {
   title: string;
   publishAt: string;
@@ -14,11 +16,14 @@ export function ArticleHead({ title, publishAt, estimatedReadingTime }: Props) {
         <h1 className="font-semibold text-lg sm:text-3xl leading-[150%] sm:leading-[170%] mb-5">
           {title}
         </h1>
-        <div className="flex flex-col justify-start items-start gap-2">
+        <div className="flex justify-start items-center gap-4">
           <span className="text-2xs">{publishAt}</span>
+          <span className="h-5 border-l" />
           <span className="text-2xs">
             {`${estimatedReadingTime.label}: ${estimatedReadingTime.value}`}
           </span>
+          <span className="h-5 border-l" />
+          <CopyUrlButton className="sm:text-xl"/>
         </div>
         <span className="border-b w-full block mt-5" />
       </div>
