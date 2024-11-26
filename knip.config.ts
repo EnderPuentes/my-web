03f6395827dcffea4958aa0b3f509f610cbe8830
Ender Puentes <endpuent@gmail.com>
2024-11-25 23:19:07 -0300

    chore(packeges): remove lucide-react package unused

--- knip.config.ts
+++ knip.config.ts
@@ -1,7 +1,7 @@
 const config = {
   ignore: ['./sanity.cli.ts', './src/components/ui/**/*'],
   ignoreBinaries: ['sync-dotenv', 'only-allow'],
-  ignoreDependencies: ['@commitlint/config-conventional', 'lucide-react'],
+  ignoreDependencies: ['@commitlint/config-conventional'],
 };
 
 export default config;
