0960ad351e51cd16a23064c5fc89fa63668822fc
Ender Puentes <endpuent@gmail.com>
2024-12-03 09:56:52 -0300

    chore(sitemap): add blog page and refactor logic

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,52 +3,55 @@ import { MetadataRoute } from 'next';
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? 'https://enderpuentes.com';
 
-type Item = {
+type Page = {
   url: string;
   lastModified: string;
   priority: number;
 };
 
-const items: Item[] = [
-  {
-    url: `${BASE_URL}/en`,
-    lastModified: new Date().toISOString(),
-    priority: 1,
-  },
-  {
-    url: `${BASE_URL}/es`,
-    lastModified: new Date().toISOString(),
-    priority: 1,
-  },
-  {
-    url: `${BASE_URL}/en/logbook`,
-    lastModified: new Date().toISOString(),
-    priority: 0.9,
-  },
-  {
-    url: `${BASE_URL}/es/logbook`,
-    lastModified: new Date().toISOString(),
-    priority: 0.9,
-  },
-];
+const lang = ['en', 'es'];
+const pages = ['', 'logbook', 'blog'];
+
+function getStaticPages(): Page[] {
+  const items: Page[] = [];
+
+  pages.forEach((page) => {
+    lang.forEach((lang) => {
+      const url = `${BASE_URL}/${lang}/${page}`;
+      const urlExist = items.find((item) => item.url === url);
+
+      if (urlExist) return;
+
+      items.push({
+        url,
+        lastModified: new Date().toISOString(),
+        priority: page === '' ? 1 : 0.9,
+      });
+    });
+  });
+
+  return items;
+}
 
 export const revalidate = 3600;
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const pages = getStaticPages();
+
   const blogArticles = await getBlogArticlesForSitemap();
 
   blogArticles?.forEach((article) => {
     const url = `${BASE_URL}/${article.lang}/${article.slug}`;
-    const urlExist = items.find((item) => item.url === url);
+    const urlExist = pages.find((item) => item.url === url);
 
     if (urlExist) return;
 
-    items.push({
+    pages.push({
       url,
       lastModified: new Date().toISOString(),
       priority: 0.7,
     });
   });
 
-  return items;
+  return pages;
 }
