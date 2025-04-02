a1bb9f38f5fb60801038435bf50261c8ba9b8394
Ender Puentes <endpuent@gmail.com>
2025-04-01 22:51:57 -0300

    refactor(identity): remove unused components and return null

--- src/components/sections/logbook/identity/index.tsx
+++ src/components/sections/logbook/identity/index.tsx
@@ -1,27 +1,7 @@
-import {
-  CopyUrlButton,
-  DownloadUrlButton,
-} from '@/components/ui/action-button';
-import { urlFor } from '@/services/sanity/lib/file';
-import { IdentitySchema } from '@/services/sanity/parser';
+import { IdentitySchema } from "@/services/sanity/parser";
 
 type Props = { data: IdentitySchema };
 
 export function Identity({ data }: Props) {
-  return (
-    <section className="bg-slate-100 bg-galaxy-gradient-light dark:bg-galaxy-gradient-dark -mt-20 pt-20">
-      <div className="container">
-        <div className="py-8 sm:py-16">
-          <h1 className="text-2xl sm:text-4xl font-bold mb-2">{data.name}</h1>
-          <h2 className="text-sm sm:text-lg mb-2 dark:text-gray-200">
-            {data.role}
-          </h2>
-          <div className="flex justify-start items-center gap-3">
-            <CopyUrlButton />
-            <DownloadUrlButton url={urlFor(data.summary)} />
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+  return null;
 }
