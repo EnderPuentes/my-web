65ef9c54e15e6c9a8a9c4d4e0a705504c52110d6
Ender Puentes <endpuent@gmail.com>
2025-04-01 23:42:06 -0300

    style(footer): revert footer background color to default for consistency

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -12,7 +12,7 @@ type Props = { data: FooterSchema | null | undefined };
 export function Footer({ data }: Props) {
   const date = new Date();
   return (
-    <footer className="dark:bg-secondary">
+    <footer className="bg-secondary">
       <div className="flex flex-col justify-center items-center gap-10 py-16 sm:py-28 px-5">
         <h1 className="text-lg sm:text-xl text-center">
           {data?.socialMedia.title}
